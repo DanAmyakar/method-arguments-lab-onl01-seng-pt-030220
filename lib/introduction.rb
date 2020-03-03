cee6020a3ffdf3b1780ed91a8da290977d19da5d
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-03 16:35:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
-def introduction(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name}. I am learning to program in #{language}."
 end
+
+introduction_with_language(Dan, Ruby)
+introduction_with_language(Jared, Java)
+introduction_with_language(Nissa, HTML_CSS)
